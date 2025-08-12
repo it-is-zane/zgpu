829cedd3ed1089f051fc3df41ff1ce3a49fb9c13
it-is-zane <zane.gant@gmail.com>
2025-08-12 15:26:30 -0700

    streamlined atlas data

--- src/math/mod.rs
+++ src/math/mod.rs
@@ -1,52 +1,81 @@
-pub mod vectors {
+pub trait Zero {
+    const ZERO: Self;
+}
+
+pub trait Identity {
+    const IDENTITY: Self;
+}
+
+pub trait Sqrt {
+    fn sqrt(self) -> Self;
+}
+
+pub trait Atan2 {
+    fn atan2(self, other: Self) -> Self;
+}
+
+macro_rules! number {
+    ($type: ty, $zero: expr, $identity: expr) => {
+        impl Zero for $type {
+            const ZERO: Self = $zero;
+        }
+
+        impl Identity for $type {
+            const IDENTITY: Self = $identity;
+        }
+    };
+}
+
+macro_rules! sqrt {
+    (float, $($type: ty)+) => {
+        $(
+            impl Sqrt for $type {
+                fn sqrt(self) -> Self {
+                    self.sqrt()
+                }
+            }
+        )+
+    };
+    (int, $($type: ty)+) => {
+        $(
+            impl Sqrt for $type {
+                fn sqrt(self) -> Self {
+                    self.isqrt()
+                }
+            }
+        )+
+    };
+}
+
+macro_rules! atan2 {
+    ($($type: ty)+) => {
+        $(
+            impl Atan2 for $type {
+                fn atan2(self, other: Self) -> Self {
+                    self.atan2(other)
+                }
+            }
+        )+
+    };
+}
+
+sqrt!(float, f32 f64);
+sqrt!(int, i32 i64 u32 u64);
+
+number!(i32, 0, 1);
+number!(i64, 0, 1);
+number!(u32, 0, 1);
+number!(u64, 0, 1);
+number!(f32, 0.0, 1.0);
+number!(f64, 0.0, 1.0);
+
+atan2!(f32 f64);
+
+pub mod vector {
+    use super::{Identity, Sqrt, Zero};
     use crate::util::{as_u8_slice, as_u8_slice_from_slice, AsBytes};
     use serde::{Deserialize, Serialize};
     use std::ops::{Add, AddAssign, Div, DivAssign, Mul, MulAssign, Neg, Sub, SubAssign};
-
-    pub trait Zero {
-        const ZERO: Self;
-    }
-
-    pub trait Identity {
-        const IDENTITY: Self;
-    }
-
-    pub trait Sqrt {
-        fn sqrt(self) -> Self;
-    }
-
-    macro_rules! sqrt {
-        (float, $($type: ty)+) => {
-            $(
-                impl Sqrt for $type {
-                    fn sqrt(self) -> Self {
-                        self.sqrt()
-                    }
-                }
-            )+
-        };
-        (int, $($type: ty)+) => {
-            $(
-                impl Sqrt for $type {
-                    fn sqrt(self) -> Self {
-                        self.isqrt()
-                    }
-                }
-            )+
-        };
-    }
-
-    macro_rules! number {
-        ($type: ty, $zero: expr, $identity: expr) => {
-            impl Zero for $type {
-                const ZERO: Self = $zero;
-            }
-
-            impl Identity for $type {
-                const IDENTITY: Self = $identity;
-            }
-        };
-    }
 
     macro_rules! create_vec {
         ($name: ident, $($fields: ident)+) => {
@@ -58,7 +87,7 @@ pub mod vectors {
 
             impl<T> $name<T> {
                 #[inline(always)]
-                pub fn new($($fields: T,)+) -> Self {
+                pub const fn new($($fields: T,)+) -> Self {
                     Self {
                         $($fields,)+
                     }
@@ -74,8 +103,8 @@ pub mod vectors {
 
             impl<T: Mul<Output = T> + Add<Output = T> + Zero> $name<T> {
                 #[inline(always)]
-                pub fn dot(self, rhs: Self) -> T {
-                    (self * rhs).sum()
+                pub fn dot(self, other: Self) -> T {
+                    (self * other).sum()
                 }
             }
 
@@ -83,6 +112,40 @@ pub mod vectors {
                 #[inline(always)]
                 pub fn len(self) -> T {
                     self.dot(self).sqrt()
+                }
+            }
+
+            impl<T: Add<Output = T> + Div<Output = T> + Zero + Mul<Output = T> + Copy> $name<T> {
+                #[inline(always)]
+                pub fn proj(self, to: Self) -> Self {
+                    to * (self.dot(to) / to.dot(to))
+                }
+            }
+
+            impl<T: Add<Output = T> + Div<Output = T> + Mul<Output = T> + Zero + Sqrt + Eq + Copy> $name<T> {
+                #[inline(always)]
+                pub fn normalized(self) -> Self {
+                    let len = self.len();
+
+                    if len == T::ZERO {
+                        self / len
+                    } else {
+                        Self::ZERO
+                    }
+
+                }
+            }
+
+            impl<T: Add<Output = T> + Div<Output = T> + Mul<Output = T> + Zero + Sqrt + PartialOrd + Copy> $name<T> {
+                #[inline(always)]
+                pub fn limit_length(self, limit: T) -> Self {
+                    let len = self.len();
+
+                    if len > limit {
+                        self / self.len()
+                    } else {
+                        self
+                    }
                 }
             }
 
@@ -235,17 +298,160 @@ pub mod vectors {
         };
     }
 
-    sqrt!(float, f32 f64);
-    sqrt!(int, i32 i64 u32 u64);
-
-    number!(i32, 0, 1);
-    number!(i64, 0, 1);
-    number!(u32, 0, 1);
-    number!(u64, 0, 1);
-    number!(f32, 0.0, 1.0);
-    number!(f64, 0.0, 1.0);
+    impl<T: Mul<Output = T> + Add<Output = T> + Sub<Output = T> + Copy> Vec3<T> {
+        #[inline(always)]
+        pub fn cross(self, rhs: Self) -> Self {
+            Self {
+                x: self.y * rhs.z - self.z * rhs.y,
+                y: self.z * rhs.x - self.x * rhs.z,
+                z: self.x * rhs.y - self.y * rhs.x,
+            }
+        }
+    }
 
     create_vec!(Vec2, x y);
     create_vec!(Vec3, x y z);
     create_vec!(Vec4, x y z w);
+}
+
+mod matrix {
+    use super::vector::{Vec2, Vec3};
+    use super::{vector::Vec4, Identity, Zero};
+    use std::ops::{Add, Mul};
+
+    macro_rules! create_matrix {
+        ($name: ident, $vec: ident, $($field: ident)+) => {
+            struct $name<T> {
+                $(pub $field: $vec<T>,)+
+            }
+
+            impl<T: Zero> Zero for $name<T> {
+                const ZERO: Self = Self {
+                    $($field: $vec::ZERO,)+
+                };
+            }
+
+            impl<T: Add<Output = T> + Mul<Output = T> + Copy + Zero> Mul<$vec<T>> for $name<T> {
+                type Output = $vec<T>;
+
+                #[inline(always)]
+                fn mul(self, rhs: $vec<T>) -> Self::Output {
+                    Self::Output {
+                        $($field: rhs.dot(self.$field),)+
+                    }
+                }
+            }
+        };
+    }
+
+    create_matrix!(Mat2, Vec2, x y);
+    create_matrix!(Mat3, Vec3, x y z);
+    create_matrix!(Mat4, Vec4, x y z w);
+
+    impl<T: Identity + Zero> Identity for Mat2<T> {
+        const IDENTITY: Self = Self {
+            x: Vec2::new(T::IDENTITY, T::ZERO),
+            y: Vec2::new(T::ZERO, T::IDENTITY),
+        };
+    }
+
+    impl<T: Identity + Zero> Identity for Mat3<T> {
+        const IDENTITY: Self = Self {
+            x: Vec3::new(T::IDENTITY, T::ZERO, T::ZERO),
+            y: Vec3::new(T::ZERO, T::IDENTITY, T::ZERO),
+            z: Vec3::new(T::ZERO, T::ZERO, T::IDENTITY),
+        };
+    }
+
+    impl<T: Identity + Zero> Identity for Mat4<T> {
+        const IDENTITY: Self = Self {
+            x: Vec4::new(T::IDENTITY, T::ZERO, T::ZERO, T::ZERO),
+            y: Vec4::new(T::ZERO, T::IDENTITY, T::ZERO, T::ZERO),
+            z: Vec4::new(T::ZERO, T::ZERO, T::IDENTITY, T::ZERO),
+            w: Vec4::new(T::ZERO, T::ZERO, T::ZERO, T::IDENTITY),
+        };
+    }
+}
+
+mod quaternion {
+    use super::{vector::Vec3, Atan2, Identity, Sqrt, Zero};
+    use std::ops::{Add, Div, Mul};
+
+    pub struct Quat<T>(Vec3<T>, T);
+
+    impl<
+            T: Add<Output = T>
+                + Mul<Output = T>
+                + Div<Output = T>
+                + Identity
+                + Zero
+                + Sqrt
+                + Atan2
+                + Eq
+                + Copy,
+        > Quat<T>
+    {
+        #[inline(always)]
+        fn to_axis_angle(self) -> Vec3<T> {
+            let len = self.0.len();
+
+            if len == T::ZERO {
+                Vec3::ZERO
+            } else {
+                (self.0 / len) * (T::IDENTITY + T::IDENTITY) * len.atan2(self.1)
+            }
+        }
+    }
+
+    // struct Quat<T = f32> {
+    //     a: T,
+    //     b: T,
+    //     c: T,
+    //     d: T,
+    // }
+}
+
+mod sdf {
+    use super::vector::{Vec2, Vec3};
+
+    impl Vec2 {
+        pub fn rect(self, pos: Vec2, scale: Vec2) -> f32 {
+            let d = Vec2::new((self.x - pos.x).abs(), (self.y - pos.y).abs()) - scale;
+
+            (Vec2::new(d.x.max(0.0), d.y.max(0.0))).len() + d.x.max(d.y).min(0.0)
+        }
+
+        pub fn polygon(self, points: Vec<Vec2>) -> f32 {
+            let d = self - points[0];
+            let mut d = d.dot(d);
+            let mut s = 1.0;
+            let mut j = points.len() - 1;
+
+            for i in 0..points.len() {
+                let e = points[j] - points[i];
+                let w = self - points[i];
+                let b = w - w.proj(e).limit_length(1.0);
+                d = d.min(b.dot(b));
+                let c = Vec3::new(
+                    self.y >= points[i].y,
+                    self.y < points[j].y,
+                    e.x * w.y > e.y * w.x,
+                );
+
+                if (c.x && c.y && c.z) || (!c.x && !c.y && !c.z) {
+                    s *= -1.0;
+                }
+            }
+
+            return s * d.sqrt();
+        }
+    }
+}
+
+fn scratch() {
+    use quaternion::Quat;
+    use vector::Vec4;
+
+    let v = Vec4::IDENTITY;
+    v.limit_length(0.5);
 }
--- src/text.rs
+++ src/text.rs
@@ -1,7 +1,6 @@
-use std::ops::{Add, Div, Mul, Sub};
-
-use crate::math::vectors::Vec2;
+use crate::math::vector::Vec2;
 use serde::{Deserialize, Serialize};
+use std::ops::Add;
 
 pub static ATLAS: std::sync::LazyLock<image::RgbaImage> = std::sync::LazyLock::new(|| {
     image::load_from_memory(include_bytes!("atlas.png"))
@@ -9,110 +8,28 @@ pub static ATLAS: std::sync::LazyLock<image::RgbaImage> = std::sync::LazyLock::n
         .to_rgba8()
 });
 
-static ATLAS_DATA: std::sync::LazyLock<std::collections::HashMap<char, CharData>> =
-    std::sync::LazyLock::new(|| {
-        toml::from_str::<std::collections::HashMap<char, Sprite>>(include_str!("atlas.toml"))
-            .unwrap()
-            .iter()
-            .map(|(char, sprite)| (*char, sprite.into()))
-            .collect()
-    });
+static ATLAS_DATA: std::sync::LazyLock<AtlasData> =
+    std::sync::LazyLock::new(|| toml::from_str(include_str!("atlas.toml")).unwrap());
 
+#[derive(Debug, Serialize, Deserialize, Clone)]
 struct AtlasData<T = f32> {
-    width: T,
-    height: T,
-    char_data: std::collections::HashMap<char, CharData>,
+    size: Vec2<T>,
+    font_size: T,
+    char_data: std::collections::HashMap<String, CharData>,
 }
 
+#[derive(Debug, Serialize, Deserialize, Copy, Clone)]
 struct CharData<T = f32> {
     tex_rect: TexRect2D<T>,
     advance: T,
 }
 
-#[derive(Debug, Copy, Clone)]
+#[derive(Debug, Copy, Clone, Serialize, Deserialize)]
 struct TexRect2D<T = f32> {
     start: Vec2<T>,
     end: Vec2<T>,
     start_uv: Vec2<T>,
     end_uv: Vec2<T>,
-}
-
-#[derive(Deserialize, Serialize, Debug, Clone, Copy)]
-#[serde(rename_all = "camelCase")]
-struct Position {
-    x: i32,
-    y: i32,
-}
-
-#[derive(Deserialize, Serialize, Debug, Clone, Copy)]
-#[serde(rename_all = "camelCase")]
-struct Size {
-    width: u32,
-    height: u32,
-}
-
-#[derive(Deserialize, Serialize, Debug, Clone, Copy)]
-#[serde(rename_all = "camelCase")]
-struct Rectangle {
-    x: u32,
-    y: u32,
-    #[serde(alias = "sizeX")]
-    width: u32,
-    #[serde(alias = "sizeY")]
-    height: u32,
-}
-
-#[derive(Deserialize, Serialize, Debug, Clone, Copy)]
-#[serde(rename_all = "camelCase")]
-struct Character {
-    value: u32,
-    offset: Position,
-    advance_x: u32,
-}
-
-#[derive(Deserialize, Serialize, Debug, Clone)]
-#[serde(rename_all = "camelCase")]
-struct Sprite {
-    name_id: String,
-    tag: String,
-    origin: Position,
-    position: Position,
-    source_size: Size,
-    padding: u32,
-    trimmed: bool,
-    trim_rec: Rectangle,
-    collider_type: u32,
-    collider_info: Rectangle,
-    char: Character,
-}
-
-impl From<&Sprite> for CharData {
-    fn from(value: &Sprite) -> Self {
-        let scale = Vec2::new(ATLAS.width() as f32, ATLAS.height() as f32);
-
-        Self {
-            tex_rect: TexRect2D {
-                start: Vec2::new(
-                    value.trim_rec.x as f32 + value.char.offset.x as f32,
-                    -(value.trim_rec.height as f32 + value.char.offset.y as f32),
-                ) / scale,
-                end: Vec2::new(
-                    value.trim_rec.width as f32 + value.char.offset.x as f32,
-                    -(value.trim_rec.y as f32 + value.char.offset.y as f32),
-                ) / scale,
-                start_uv: Vec2::new(
-                    (value.position.x) as f32,
-                    (value.source_size.height as i32 + value.position.y) as f32,
-                ) / scale,
-                end_uv: Vec2::new(
-                    (value.source_size.width as i32 + value.position.x) as f32,
-                    (value.position.y) as f32,
-                ) / scale,
-            },
-
-            advance: value.char.advance_x as f32 / scale.x,
-        }
-    }
 }
 
 impl<T: Add<Output = T> + Copy> Add<Vec2<T>> for TexRect2D<T> {
@@ -122,45 +39,6 @@ impl<T: Add<Output = T> + Copy> Add<Vec2<T>> for TexRect2D<T> {
         TexRect2D {
             start: self.start + rhs,
             end: self.end + rhs,
-            start_uv: self.start_uv,
-            end_uv: self.end_uv,
-        }
-    }
-}
-
-impl<T: Sub<Output = T> + Copy> Sub<Vec2<T>> for TexRect2D<T> {
-    type Output = TexRect2D<T>;
-
-    fn sub(self, rhs: Vec2<T>) -> Self::Output {
-        TexRect2D {
-            start: self.start - rhs,
-            end: self.end - rhs,
-            start_uv: self.start_uv,
-            end_uv: self.end_uv,
-        }
-    }
-}
-
-impl<T: Mul<Output = T> + Copy> Mul<T> for TexRect2D<T> {
-    type Output = TexRect2D<T>;
-
-    fn mul(self, rhs: T) -> Self {
-        TexRect2D {
-            start: self.start * rhs,
-            end: self.end * rhs,
-            start_uv: self.start_uv,
-            end_uv: self.end_uv,
-        }
-    }
-}
-
-impl<T: Div<Output = T> + Copy> Div<T> for TexRect2D<T> {
-    type Output = TexRect2D<T>;
-
-    fn div(self, rhs: T) -> Self {
-        TexRect2D {
-            start: self.start / rhs,
-            end: self.end / rhs,
             start_uv: self.start_uv,
             end_uv: self.end_uv,
         }
@@ -192,7 +70,7 @@ impl Line {
         self.tex_rects = text
             .chars()
             .filter_map(|c| {
-                let data = atlas.get(&c);
+                let data = atlas.char_data.get(&c.to_string());
                 println!("{c}:{}", data.unwrap().advance);
                 data
             })
@@ -212,7 +90,7 @@ impl Line {
         let mut new = text
             .chars()
             .filter_map(|c| {
-                let data = atlas.get(&c);
+                let data = atlas.char_data.get(&c.to_string());
                 println!("{c}:{}", data.unwrap().advance);
                 data
             })
